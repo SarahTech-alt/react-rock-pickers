04bc4c2166cea135840d30e0095c6729b4519d64
Sarah Fuoss <sfuoss@gmail.com>
2021-09-14 13:31:47 -0500

    Completed base mode.

--- src/components/RockCounter/RockCounter.js
+++ src/components/RockCounter/RockCounter.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
+const useState = React.useState;
+
+
 function RockCounter() {
+  const [rockCounter, setCount] = useState(0);
+
   return (
     <div>
-      <div>Rocks Picked: 0</div>
+      <div>Rocks Picked: {rockCounter} </div>
       <div>
-        <button>Increase</button>
-        <button>Decrease</button>
-        <button>Reset</button>
+        <button onClick={() => setCount(rockCounter +1)}>Increase</button>
+        <button onClick={()=> setCount(rockCounter - 1)}>Decrease</button>
+        <button onClick ={() => setCount(0)}>Reset</button>
       </div>
     </div>
   );
